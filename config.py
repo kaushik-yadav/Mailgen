85aca91eccc4e4159d718f7946f4cd6453a5986e
kaushik-yadav <kaushiky005@gmail.com>
2025-06-27 04:10:41 +0530

    feat: Add a better model

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 TOGETHER_API_BASE = "https://api.together.xyz/v1"
-MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+MODEL = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
